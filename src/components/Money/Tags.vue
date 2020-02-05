fe4f84048a1f281a4b0389bc5f5c972e4a851b62
70kg111 <76104718@qq.com>
2020-02-05 14:19:54 +0800

    把Money.vue分成4个子组件

---
+++ src/components/Money/Tags.vue
@@ -0,0 +1,61 @@
+<template>
+    <div class="tags">
+        <div class="new">
+            <button>新增标签</button>
+        </div>
+        <ul class="current">
+            <li>衣</li>
+            <li>食</li>
+            <li>住</li>
+            <li>行</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "Tags"
+  }
+</script>
+
+<style lang="scss" scoped>
+    .tags {
+        font-size: 14px;
+        padding: 16px;
+        flex-grow: 1;
+        display: flex;
+        flex-direction: column-reverse;
+
+        > .current {
+            display: flex;
+            flex-wrap: wrap;
+            overflow: auto;
+
+            > li {
+                margin-top: 4px;
+                display: flex;
+                justify-content: center;
+                align-items: center;
+                background: #d9d9d9;
+                padding: 0 16px;
+                height: 24px;
+                border-radius: (24px/2);
+                text-align: center;
+                margin-right: 12px;
+            }
+        }
+
+        > .new {
+            padding-top: 16px;
+
+            > button {
+                background: transparent;
+                border: none;
+                border-bottom: 1px solid;
+                color: #999999;
+                padding: 0 3px;
+            }
+        }
+    }
+
+</style>
